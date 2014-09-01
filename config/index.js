2c6e7e3423f41d904ab11fc1b5fe061a59340eeb
albin3 <albin@louding.com>
2014-09-01 14:37:57 +0800

    增加mime和浏览器缓存技术

--- config/index.js
+++ config/index.js
@@ -1,5 +1,12 @@
 
 module.exports = {
-  "upload": "pub",
-  "port": 3015
-}
+  "upload": "pub"
+ ,"port": 3015
+ ,"expires": {
+      "fileMatch": /^(gif|png|jpg|jpeg|js|css)$/ig,
+      "maxAge": 60 * 60 *24 * 365
+  }
+ ,"compress": {
+      "match": /css|js|html/ig
+ }
+};
